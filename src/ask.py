bc37b44597d8fd4b556765c53ad0711348e799c8
Denzel Tajo <42041797+dnzltajo@users.noreply.github.com>
2020-05-17 11:05:28 +0800

    Update ask.py

--- src/ask.py
+++ src/ask.py
@@ -17,7 +17,7 @@ buf2 = array('H', 2048 + int(2047 * (0.25*(math.sin(2 * math.pi * i /256)))) for
 dac = DAC(1, bits=12)
 
 #Simulates a stream of Data
-stream = (0,1,0,1,0,1)
+stream = (0,1,0,1,1,1)
 
 def ask():
     #Logic 1 signal
